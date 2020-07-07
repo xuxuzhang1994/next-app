b69864034ac17ab1f52130bfa093c102549eaac9
jiayu <xuxu_zhang09@163.com>
2020-07-07 16:45:45 +0800

    1

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,40 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
+
+  const executeCalliOS = () => {
+    var iframe = document.createElement("IFRAME");
+    iframe.setAttribute("src", url);
+    document.body.appendChild(iframe);
+    iframe.parentNode.removeChild(iframe);
+    iframe = null;
+  }
+
+  /*iOS跳转功能方法
+     *func_id为功能ID
+     *para 为参数，形式如 fund_market=xxxx&fund_name=xxxx&fund_code=xxxx
+     */
+  const jumpIOS = (func_id,para) => {
+      executeCalliOS("innerAppCall://CCBCallCenter?funcid="+func_id+"&"+para)
+  }
+
+
+  const openHref = () => {
+    jumpIOS('07011040', 'ZH072011004000Y01')
+  }
+
+
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+      <div onClick={openHref}>链接1</div>
+      <div>链接1</div>
+      <div>链接1</div>
+      <div>链接1</div>
+      <div>链接1</div>
+      <Link href="/test">
+        <h1>title</h1>
+      </Link>
 
       <style jsx>{`
         .container {
